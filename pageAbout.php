e83c48ab631dba7f1782ff6fe5d3a9379b0cb02d
Justin Li <justin355li@gmail.com>
2024-04-07 05:49:44 -0400

    Update about and RG page

--- pageAbout.php
+++ pageAbout.php
@@ -1,1 +1,18 @@
-<h1>under development...</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>About Page</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+
+<body>
+    <?php include 'connectdb.php'; ?>
+
+    <?php include 'navbar.php'; ?>
+    
+    <h1><i>About page</i> is under development...</h1>
+
+    <?php $connection = NULL; ?>
+</body>
--- pageRentalGroups.php
+++ pageRentalGroups.php
@@ -1,1 +1,18 @@
-<h1>under development...</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>All Rental Groups</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+
+<body>
+    <?php include 'connectdb.php'; ?>
+
+    <?php include 'navbar.php'; ?>
+    
+    <h1><i>Rental groups</i> page is under development...</h1>
+
+    <?php $connection = NULL; ?>
+</body>
